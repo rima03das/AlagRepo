d9d3abd263ea9fd9ffaf049d4af0fb15142858af
Rima <rima03das@gmail.com>
2014-09-15 13:34:57 +0530

    First Alag Commit

---
+++ AlagPro/src/cmc/alag/pkg/AlagActionOne.java
@@ -0,0 +1,14 @@
+package cmc.alag.pkg;
+
+import com.opensymphony.xwork2.Action;
+
+public class AlagActionOne implements Action{
+
+	@Override
+	public String execute() throws Exception {
+
+		
+		return "one";
+	}
+
+}
