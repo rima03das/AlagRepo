975cc8727167617c165370ae94fc47c483485dba
Project X <Project X@192.168.137.27>
2014-09-15 14:11:38 +0530

    Adding new jsp

--- AlagPro/src/cmc/alag/pkg/AlagActionOne.java
+++ AlagPro/src/cmc/alag/pkg/AlagActionOne.java
@@ -4,9 +4,24 @@ import com.opensymphony.xwork2.Action;
 
 public class AlagActionOne implements Action {
 
+	
+	String username;
+	
 	@Override
 	public String execute() throws Exception {
 
-		return "one";
+		if(username.equalsIgnoreCase("one")){
+			return "one";
+		}else{
+			return "two";
+		}
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 }
